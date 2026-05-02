b85c76210814da387ebe24888053b75210b23065
FUJI <jazzmaster204@gmail.com>
2026-05-02 16:24:36 +0300

    [aop] #7-5 >>>> /practice / @Around

--- sprin9/src/main/java/org/aop/practice8/aop/Aop7.java
+++ sprin9/src/main/java/org/aop/practice8/aop/Aop7.java
@@ -2,13 +2,17 @@ package org.aop.practice8.aop;
 
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class Aop7 {
-    @After("!execution(* org.aop.practice8.services.pac1..*(..))")
-    public void excludePac1 () {
+    @After("excludePac()")
+    public void excluding () {
         System.out.println("excluding package methods");
     }
+
+    @Pointcut("execution(* org.aop.practice8.services.pac1..*(..))")
+    public void excludePac () {}
 }
---
+++ sprin9/src/main/java/org/aop/practice8/starter/Main8.java
@@ -0,0 +1,24 @@
+package org.aop.practice8.starter;
+
+import org.aop.practice8.config.Config;
+import org.aop.practice8.services.UserService;
+import org.aop.practice8.services.pac1.PackageService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main8 {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(Config.class);
+
+        try(context) {
+            PackageService packageService = context.getBean(PackageService.class);
+            packageService.packing();
+
+            UserService userService = context.getBean(UserService.class);
+            userService.register("dada");
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
